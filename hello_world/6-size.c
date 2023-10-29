8195fb90c63778c2d2dc03a5e6fa9c4e96a96dba
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-10-29 13:43:34 -0700

    C - Hello, World task 6 v2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,17 +7,10 @@
  */
 int main(void)
 {
-    printf("The size of int is %zu bytes\n", sizeof(int));
-    printf("The size of char is %zu byte\n", sizeof(char));
-    printf("The size of float is %zu bytes\n", sizeof(float));
-    printf("The size of double is %zu bytes\n", sizeof(double));
-    printf("The size of long is %zu bytes\n", sizeof(long));
-    printf("The size of long long is %zu bytes\n", sizeof(long long));
-    printf("The size of short is %zu bytes\n", sizeof(short));
-    printf("The size of unsigned int is %zu bytes\n", sizeof(unsigned int));
-    printf("The size of unsigned long is %zu bytes\n", sizeof(unsigned long));
-    printf("The size of unsigned long long is %zu bytes\n", sizeof(unsigned long long));
-    printf("The size of unsigned short is %zu bytes\n", sizeof(unsigned short));
-
-    return 0;
+printf("The size of int is %zu bytes\n", sizeof(int));
+printf("The size of char is %zu byte\n", sizeof(char));
+printf("The size of float is %zu bytes\n", sizeof(float));
+printf("The size of long is %zu bytes\n", sizeof(long int));
+printf("The size of long long is %zu bytes\n", sizeof(long long));
+return (0);
 }
