dc7fe4277107b3f19419b26d05148649653e59c9
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-10-31 15:28:27 -0700

    shell init files task 6 v1

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+  * main - entry point
+  * Description: Validate last digit
+  * n: the number to be checked
+  * Return: Always 0 (Succes)
+  */
+int main(void)
+{
+	int n;
+
+	n = '0';
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar ('\n');
+	return (0);
+}
