5e6298f3645ec4e12fefb7adc03992266f5fa95b
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-02 14:08:18 -0700

    C - Functions, nested loops task 3 v1

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _islower - checks for lowercase
+*@c: the letter to be checked
+*
+* Description: checks for lowercase, if is lowercase print 1
+* Return: 1 if it is lowercase. 0 otherwise
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
