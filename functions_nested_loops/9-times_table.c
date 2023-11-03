0f5626266e06bcfb4623342a643665aaeb5dec96
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-03 14:02:43 -0700

    C - Functions, nested loops task 9 v3

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,34 +7,32 @@
 */
 void times_table(void)
 {
-	int row, column, product;
+	int row, column, product, nproduct;
 
 	for (row = 0; row <= 9; row++)
 	{
 		for (column = 0; column <= 9; column++)
 		{
 			product = row * column;
+			nproduct = (column + 1) * row;
 			if (product <= 9)
 			{
 				_putchar(product + '0');
-				if (column <= 8)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
 			}
 			else
 			{
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
-				if (column <= 8)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			}
-
+			if (column <= 8)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (nproduct <= 9)
+			{
+				_putchar(' ');
+			}
 		}
 	_putchar ('\n');
 	}
