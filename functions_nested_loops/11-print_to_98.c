f2f1747cdb07342b8d99831a63c673fc8c0626fe
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-03 16:50:35 -0700

    C - Functions, nested loops task 11 v2

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,19 +8,27 @@
 */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n == 98)
+	{
+		printf("%d", n);
+	}
+	while (n <= 97)
 	{
 		printf("%d, ", n);
 		n++;
+		if (n == 98)
+		{
+			printf("%d", n);
+		}
 	}
 	while (n >= 99)
 	{
 		printf("%d, ", n);
 		n--;
-	}
-	if (n == 98)
-	{
-		printf("%d", n);
+		if (n == 98)
+		{
+			printf("%d", n);
+		}
 	}
 	putchar('\n');
 }
