ee36e14c4fd5b3e6b62f1edd9fce1a01ecf60c6b
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-03 16:22:53 -0700

    C - Functions, nested loops task 11 v1

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_to_98 - print n to 98
+* @n: variable to be print
+*
+* Description: Print 9 times table in specific format
+*/
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n >= 99)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	if (n == 98)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+}
