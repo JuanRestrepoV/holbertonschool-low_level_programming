e82024b6e57ed89fdc3d457cea46d82971ca0c2c
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-03 15:15:48 -0700

    C - Functions, nested loops task 10 v1

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* add - add two integers
+*
+* Description: add two integers and return result
+* Return: the sum of the two parameters
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
