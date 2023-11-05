51fa6381e4a506d60add43a3dc3c66fd109ecb31
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-05 10:57:51 -0800

    C - More functions, more nested loops task 2 v1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+* mul - multiply two integer
+* @a: first operator
+* @b: second operator
+*
+* Description: multiply a * b
+* Return: 0 if succes
+*/
+int mul(int a, int b)
+{
+	return(a * b);
+}
---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+* print_numbers - print form 0 to 9
+*
+* Description: print from 0 to 9
+*/
+void print_numbers(void)
+{
+	int n;
+
+	n = 0;
+	while (n >= 9)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
