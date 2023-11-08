dc3b60e38adce5684b9bad93bea4a24630222a70
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-08 15:21:31 -0800

    C - Pointers, arrays and strings task 7 v2

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,12 +8,13 @@
 */
 void puts_half(char *str)
 {
-	int a;
+	int a, posit;
 
 	a = 0;
 	for (a = 0; str[a] != '\0'; a++)
 	{
 	}
+	posit = a;
 	if (a % 2 == 1)
 	{
 		a = (a - 1) / 2;
@@ -22,7 +23,7 @@ void puts_half(char *str)
 	{
 		a = (a / 2);
 	}
-	for (; str[a] < '\0'; a++)
+	for (; a != posit; a++)
 	{
 		putchar(str[a]);
 	}
