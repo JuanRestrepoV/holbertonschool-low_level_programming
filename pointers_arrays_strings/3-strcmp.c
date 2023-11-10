530905a957801a19029bbc0ece866cc296a5d3b8
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-10 12:59:45 -0800

    C - More pointers, arrays and strings task 3 v1

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,16 +6,16 @@
 *
 * Return: An integer less than, equal to, or greater than zero.
 */
-int _strcmp(char *s1, char *s2) 
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0' && *s2 != '\0') 
 	{
 		if (*s1 != *s2)
 		{
-			return *s1 - *s2;
+			return (*s1 - *s2);
 		}
 		s1++;
 		s2++;
 	}
-	return *s1 - *s2;
+	return (*s1 - *s2);
 }
