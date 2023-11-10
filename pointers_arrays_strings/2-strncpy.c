7af2922e545c3246a761a6a9fb54f587f212bcea
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-10 09:20:36 -0800

    C - More pointers, arrays and strings task 2 v1

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _strncat - function that copies a string.
+* @dest: pointer to destination string
+* @src: pointer to source string.
+* @n: number of bytes from source string that must be used to copy.
+*
+* Description: function that copies a string.
+* Return: pointer to the resulting string dest.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	a = 0;
+	while (src[a] != src[n])
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	return (dest);
+}
