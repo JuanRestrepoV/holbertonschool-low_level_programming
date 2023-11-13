5c9805ba7c9d1172be96d4debd601f35110962ca
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-13 13:09:37 -0800

    C - Recursion task 2 v1

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _strlen - returns the length of a string
+*@s: variable that contains a string
+*
+* Description: returns the length of a string.
+* Return: 0 if succes
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	_strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
+}
