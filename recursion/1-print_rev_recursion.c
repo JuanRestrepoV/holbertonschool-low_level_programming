38f288c4b1195caa80929f0f8db76865de26db4b
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-13 12:51:55 -0800

    C - Recursion task 1 v1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* _print_rev_recursion - function that prints a string in reverse.
+* @s: The string to print
+*
+* Description: function that prints a string in reverse.
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
