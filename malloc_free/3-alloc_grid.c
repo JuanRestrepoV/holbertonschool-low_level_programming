bf0b13eeb38b7d32cd5a2a8bc2770d163fe77a10
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-17 12:02:43 -0800

    C - Even more pointers, arrays and strings task 2 v2

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdlib.h>
+/**
+* *str_concat - concatenates two strings.
+* @s1: pointer to string 1 for copy into concat
+* @s2: pointer to string 2 for add to concat after s1 copy
+*
+* Description: returned pointer should point to a newly allocated space
+* in memory which contains the contents of s1 followed by the
+* contents of s2, and null terminated.
+* Return: On success 1.
+*/
+int **alloc_grid(int width, int height)
