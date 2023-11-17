6634c576f1374daf6de3e61bbbcd983ac619c6ef
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-17 12:37:57 -0800

    C - Even more pointers, arrays and strings task 2 v3

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -14,9 +14,11 @@ char *_strchr(char *s, char c)
 	int a;
 
 	for (a = 0; s[a] != c; a++)
-	if (s[a] == c)
 	{
-		return (&s[a]);
+		if (s[a] == c)
+		{
+			return (&s[a]);
+		}
 	}
 	return (NULL);
 }
