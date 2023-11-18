39e4c9556b5cc95af5beb9aeb1c6564f1686bdcd
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-17 18:40:41 -0800

    C - malloc, free task 4 v1

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 		matrix[i] = malloc(sizeof(int) * width);
 		if (matrix[i] == NULL)
 		{
-			free(matrix);
+			free(matrix[i]);
 			return (NULL);
 		}
 	}
