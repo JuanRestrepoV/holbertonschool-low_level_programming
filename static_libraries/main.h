9cb35be69f8156aa3f384e7db545d6343040cf8b
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-19 09:28:33 -0800

    C - Static libraries task 0 v1

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,3 +1,5 @@
+#ifndef headerfile
+#define headerfile
 /**
 * main - check the code
 * @c: the character to print
@@ -24,3 +26,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
