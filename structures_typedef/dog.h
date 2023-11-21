b155956278c5953985e688e352ddad9df9b8251b
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-21 14:40:28 -0800

    C - Structures, typedef task 0 v1

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE_1
+#define HEADER_FILE_1
+/**
+* struct dog - Typedef for struct.
+* @name: name of dog.
+* @age: Age of dog.
+* @owner: Owner of dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
