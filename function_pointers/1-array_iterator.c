cf5b30d2c9a9f290520a82b5ac8cb0315d08b4ff
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-11-24 12:04:04 -0800

    C - Function pointers task 1 v1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,6 +1,7 @@
 #include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 /**
 * array_iterator - function that executes a function given as a parameter 
 * on each element of an array..
@@ -8,13 +9,14 @@
 * @size: Size of the array of type size_t
 * @action: pointer to function that will be apply to the array.
 */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i;
+
 	if (array == NULL || action == NULL)
 	{
 		return;
 	}
-	size_t i;
 	/**Iteramos i mientras sea menor que el tama;o
 	del string entrara al bloque de codigo*/
 	for (i = 0; i < size; i++)
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #ifndef POINTER_FUNCTION
 #define POINTER_FUNCTION
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
