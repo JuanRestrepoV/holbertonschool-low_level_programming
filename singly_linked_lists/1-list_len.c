b52e572fda1a9a4660272ca99375fc9a402c4367
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-12-11 13:43:06 -0800

    C - Singly linked lists task 1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 /**
-* print_list - function that prints all the elements of a list_t list.
+* list_len -  function that returns the number of elements in a linked
+* list_t list.
 * @h: pointer to head node.
 *
-* Description: function that prints all the elements of a list_t list.
-* Return: number of nodes.
+* Description:  function that returns the number of elements.
+* Return: number of elements or nodes.
 */
 size_t list_len(const list_t *h)
 {
