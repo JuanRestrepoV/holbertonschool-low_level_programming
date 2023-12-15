355d01a50cc5f48eade10fcdaa434b46f50f670e
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2023-12-15 15:41:13 -0800

    C - Doubly linked lists task 8 v5

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -29,26 +29,16 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(current);
 		return (-1);
 	}
-
-	i = 0;
-	while (current != NULL && i < index)
+	for (i = 0; current != NULL && i < index; i++)
 	{
 		current = current->next;
-		i++;
 	}
 	if (current == NULL)
-	{
 		return (-1);
-	}
-
 	if (current->next != NULL)
-	{
 		current->next->prev = current->prev;
-	}
 	if (current->prev != NULL)
-	{
 		current->prev->next = current->next;
-	}
-	free(current);
+	free(current)
 	return (1);
 }
