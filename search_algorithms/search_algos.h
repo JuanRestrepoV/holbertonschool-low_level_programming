8e0c9974157135c30d4950821641c54503c38f61
JuanRestrepoV <juanpablorestrepovidal@gmail.com>
2024-02-04 16:40:10 -0800

    task 0

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,5 +1,8 @@
-#ifndef "SEARCH_ALGORITHMS"
-#define "SEARCH_ALGORITHMS"
+#ifndef SEARCH_ALGORITHMS
+#define SEARCH_ALGORITHMS
+
+#include <stdlib.h>
+#include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
